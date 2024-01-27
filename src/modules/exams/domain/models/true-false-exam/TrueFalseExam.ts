db99ba249f7ea10a6bb80a4b078361bf93954d39
Orlando Jimenez <orlandojn10@gmail.com>
2024-01-27 13:25:03 +0100

    feat: created the domain directory for exams

---
+++ src/modules/exams/domain/models/true-false-exam/TrueFalseExam.ts
@@ -0,0 +1,11 @@
+export type TrueFalseExam = {
+  topic: string;
+  subject: string;
+  examStructure: {
+    examTitle: string;
+    questions: Array<{
+      question: string;
+      answer: boolean;
+    }>;
+  };
+};
