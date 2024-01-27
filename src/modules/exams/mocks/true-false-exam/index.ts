c20a6cf942f81c9537194cdb92a4bc14e901aa2b
Orlando Jimenez <orlandojn10@gmail.com>
2024-01-27 13:43:38 +0100

    feat: created mock directory for true false exams

---
+++ src/modules/exams/mocks/true-false-exam/index.ts
@@ -0,0 +1,28 @@
+import { TrueFalseExam } from '../../domain/models/true-false-exam/TrueFalseExam';
+
+export const trueFalseExam: TrueFalseExam = {
+  topic: 'Estructuras III',
+  subject: 'Tecnología',
+  examStructure: {
+    examTitle: 'Señala si son verdaderas o falsas las siguientes frases',
+    questions: [
+      {
+        question: 'El esfuerzo cortante solo aparece cuando cortamos algo.',
+        answer: true,
+      },
+      {
+        question: 'La tracción es un esfuerzo que tiende a estirar un objeto.',
+        answer: false,
+      },
+      {
+        question:
+          'El esqueleto humano está sometido básicamente al esfuerzo de compresión.',
+        answer: true,
+      },
+      {
+        question: 'Un vaso de plástico no tiene ningún tipo de estructura.',
+        answer: false,
+      },
+    ],
+  },
+};
