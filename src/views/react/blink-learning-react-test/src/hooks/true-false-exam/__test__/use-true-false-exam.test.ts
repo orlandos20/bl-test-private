b6743c866b65cbd8b97fc394fa30be665636ea24
Orlando Jimenez <orlandojn10@gmail.com>
2024-01-28 20:09:54 +0100

    feat: test suite for useTrueFalseExam hook

--- src/views/react/blink-learning-react-test/src/hooks/true-false-exam/__test__/use-true-false-exam.test.ts
+++ src/views/react/blink-learning-react-test/src/hooks/true-false-exam/__test__/use-true-false-exam.test.ts
@@ -1,58 +1,90 @@
-import { renderHook } from '@testing-library/react';
+import { renderHook, act, RenderHookResult } from '@testing-library/react';
 import { useTrueFalseExam } from '../use-true-false-exam';
+import { trueFalseExam } from '../../../../../../../modules/exams/trueFalse/mocks';
+import { UserResponse } from '../../../../../../../modules/exams/trueFalse/domain/models/TrueFalseExam';
 
-export const multiplyNumbers = (
-  a: number,
-  b: number,
-  comparisionNumber: number
-) => {
-  const result = a * b;
-
-  if (result < 0) {
-    return 'Should be only positive values';
-  }
-
-  if (result === comparisionNumber) {
-    return 'isEqual';
-  }
-
-  if (result > comparisionNumber) {
-    return 'isGreater';
-  }
-
-  return 'isLower';
+const mockResponses = ({
+  correctAnswersOnly,
+}: {
+  correctAnswersOnly: boolean;
+}) => {
+  return trueFalseExam.examStructure.questions.map(({ question, answer }) => ({
+    question,
+    userAnswer: correctAnswersOnly ? answer : !answer,
+  }));
 };
 
-describe('should works --> ', () => {
-  it('should be Lower', async () => {
-    const result = multiplyNumbers(2, 2, 5);
-
-    await expect(result).toEqual('isLower');
-  });
-
-  it('should be Greater', async () => {
-    const result = multiplyNumbers(2, 2, 3);
-
-    await expect(result).toEqual('isGreater');
-  });
-
-  it('should be Equal', async () => {
-    const result = multiplyNumbers(2, 2, 4);
-
-    await expect(result).toEqual('isEqual');
-  });
-
-  //   'Should be only positive values'
-
-  it('should be only positives', async () => {
-    const result = multiplyNumbers(-2, 2, 4);
-
-    await expect(result).toEqual('Should be only positive values');
-  });
+const mockedCorrectUserResponses: UserResponse[] = mockResponses({
+  correctAnswersOnly: true,
 });
 
-describe('testing useTrueFalseExam hook --> ', () => {
-  it('should execute correctly --> ', () => {
-    expect(renderHook(() => useTrueFalseExam())).not.toThrow();
+// const mockedIncorrectUserResponses: UserResponse[] = mockResponses({
+//   correctAnswersOnly: false,
+// });
+
+describe('useTrueFalseExam hook ', () => {
+  test('should execute correctly ', () => {
+    const { result } = renderHook(() => useTrueFalseExam());
+    const { examIsCompleted } = result.current;
+    expect(examIsCompleted).not.toBeUndefined();
+  });
+
+  describe('testing hook initial values ', () => {
+    const { result } = renderHook(() => useTrueFalseExam());
+    test('should initialize examIsCompleted as false', () => {
+      const { examIsCompleted } = result.current;
+      expect(examIsCompleted).toBe(false);
+    });
+
+    test('should initialize examData as undefined', () => {
+      const { examData } = result.current;
+      expect(examData).toBeUndefined();
+    });
+
+    it('should initialize examResults as undefined', () => {
+      const { examResults } = result.current;
+      expect(examResults).toBeUndefined();
+    });
+  });
+
+  describe('testing exam logic', () => {
+    let renderedHook: RenderHookResult<
+      ReturnType<typeof useTrueFalseExam>,
+      undefined
+    >;
+
+    beforeEach(async () => {
+      renderedHook = renderHook(() => useTrueFalseExam());
+      const { result } = renderedHook;
+      const { getLocalExam } = result.current;
+      await act(async () => {
+        await getLocalExam();
+      });
+    });
+    test('should execute getLocalExam and set examData', async () => {
+      const { result } = renderedHook;
+      const { examData } = result.current;
+      expect(examData).toBeDefined();
+      expect(examData?.topic).toBeDefined();
+      expect(examData?.subject).toBeDefined();
+      expect(examData?.examStructure).toBeDefined();
+      expect(examData?.examStructure.questions.length).toEqual(
+        trueFalseExam.examStructure.questions.length
+      );
+    });
+
+    test('should call the submitExam method when exam is completed', async () => {
+      const { result } = renderedHook;
+      const { submitExam } = result.current;
+      await act(async () => {
+        await submitExam(mockedCorrectUserResponses);
+      });
+      const { examIsCompleted, examResults } = result.current;
+      expect(examIsCompleted).toBe(true);
+      expect(examResults).toBeDefined();
+      expect(examResults?.length).toEqual(
+        trueFalseExam.examStructure.questions.length
+      );
+    });
   });
 });
